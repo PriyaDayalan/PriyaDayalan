9417ee20eea2f9060779f7e944231ce1462fa8c5
PriyaDayalan <priyad@moback.com>
2022-08-24 10:55:47 +0530

    message

---
+++ Bike/src/org/bike/Details.java
@@ -0,0 +1,17 @@
+package org.bike;
+
+
+
+public class Details {
+	
+private void method() {
+		
+		System.out.println("1");
+
+	}
+	public static void main(String[] args) {
+		Details s=new Details();
+		s.method();
+	}
+
+}
